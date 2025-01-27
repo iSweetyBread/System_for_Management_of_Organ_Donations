855553e1956cf6610942fcb7dbbb794360386911
iSweetyBread <glyb.tg@gmail.com>
2025-01-27 02:45:50 +0100

    Add files via upload

---
+++ ui/frmMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ui
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOrgans = new System.Windows.Forms.Button();
+            this.btnDonors = new System.Windows.Forms.Button();
+            this.btnReceivers = new System.Windows.Forms.Button();
+            this.btnBdonors = new System.Windows.Forms.Button();
+            this.btnPlaces = new System.Windows.Forms.Button();
+            this.btnBlood = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOrgans
+            // 
+            this.btnOrgans.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOrgans.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrgans.Location = new System.Drawing.Point(12, 12);
+            this.btnOrgans.Name = "btnOrgans";
+            this.btnOrgans.Size = new System.Drawing.Size(360, 48);
+            this.btnOrgans.TabIndex = 0;
+            this.btnOrgans.Text = "Organ Donations";
+            this.btnOrgans.UseVisualStyleBackColor = true;
+            this.btnOrgans.Click += new System.EventHandler(this.btnOrgans_Click);
+            // 
+            // btnDonors
+            // 
+            this.btnDonors.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDonors.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDonors.Location = new System.Drawing.Point(12, 66);
+            this.btnDonors.Name = "btnDonors";
+            this.btnDonors.Size = new System.Drawing.Size(360, 48);
+            this.btnDonors.TabIndex = 1;
+            this.btnDonors.Text = "Organ Donors";
+            this.btnDonors.UseVisualStyleBackColor = true;
+            this.btnDonors.Click += new System.EventHandler(this.btnDonors_Click);
+            // 
+            // btnReceivers
+            // 
+            this.btnReceivers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReceivers.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReceivers.Location = new System.Drawing.Point(12, 120);
+            this.btnReceivers.Name = "btnReceivers";
+            this.btnReceivers.Size = new System.Drawing.Size(360, 48);
+            this.btnReceivers.TabIndex = 2;
+            this.btnReceivers.Text = "Organ Recipients";
+            this.btnReceivers.UseVisualStyleBackColor = true;
+            this.btnReceivers.Click += new System.EventHandler(this.btnReceivers_Click);
+            // 
+            // btnBdonors
+            // 
+            this.btnBdonors.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBdonors.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBdonors.Location = new System.Drawing.Point(12, 174);
+            this.btnBdonors.Name = "btnBdonors";
+            this.btnBdonors.Size = new System.Drawing.Size(360, 48);
+            this.btnBdonors.TabIndex = 3;
+            this.btnBdonors.Text = "Blood Donors";
+            this.btnBdonors.UseVisualStyleBackColor = true;
+            this.btnBdonors.Click += new System.EventHandler(this.btnBdonors_Click);
+            // 
+            // btnPlaces
+            // 
+            this.btnPlaces.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPlaces.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlaces.Location = new System.Drawing.Point(12, 228);
+            this.btnPlaces.Name = "btnPlaces";
+            this.btnPlaces.Size = new System.Drawing.Size(360, 48);
+            this.btnPlaces.TabIndex = 4;
+            this.btnPlaces.Text = "Facilities";
+            this.btnPlaces.UseVisualStyleBackColor = true;
+            this.btnPlaces.Click += new System.EventHandler(this.btnPlaces_Click);
+            // 
+            // btnBlood
+            // 
+            this.btnBlood.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBlood.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBlood.Location = new System.Drawing.Point(12, 282);
+            this.btnBlood.Name = "btnBlood";
+            this.btnBlood.Size = new System.Drawing.Size(360, 48);
+            this.btnBlood.TabIndex = 5;
+            this.btnBlood.Text = "Blood Amount";
+            this.btnBlood.UseVisualStyleBackColor = true;
+            this.btnBlood.Click += new System.EventHandler(this.btnBlood_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(12, 397);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(360, 48);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 457);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnBlood);
+            this.Controls.Add(this.btnPlaces);
+            this.Controls.Add(this.btnBdonors);
+            this.Controls.Add(this.btnReceivers);
+            this.Controls.Add(this.btnDonors);
+            this.Controls.Add(this.btnOrgans);
+            this.Name = "frmMain";
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOrgans;
+        private System.Windows.Forms.Button btnDonors;
+        private System.Windows.Forms.Button btnReceivers;
+        private System.Windows.Forms.Button btnBdonors;
+        private System.Windows.Forms.Button btnPlaces;
+        private System.Windows.Forms.Button btnBlood;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ ui/frmMain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using logic;
+using logic.infoObjects;
+
+namespace ui
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnOrgans_Click(object sender, EventArgs e)
+        {
+            frmOrgans frmOrgans = new frmOrgans();
+            frmOrgans.ShowDialog();
+        }
+
+        private void btnDonors_Click(object sender, EventArgs e)
+        {
+            frmDonors frmDonors = new frmDonors();
+            frmDonors.ShowDialog();
+        }
+
+        private void btnReceivers_Click(object sender, EventArgs e)
+        {
+            frmRecepients frmRecepients = new frmRecepients();
+            frmRecepients.ShowDialog();
+        }
+
+        private void btnBdonors_Click(object sender, EventArgs e)
+        {
+            frmBDonors frmBDonors = new frmBDonors();
+            frmBDonors.ShowDialog();
+        }
+
+        private void btnPlaces_Click(object sender, EventArgs e)
+        {
+            frmPlaces frmPlaces = new frmPlaces();
+            frmPlaces.ShowDialog();
+        }
+
+        private void btnBlood_Click(object sender, EventArgs e)
+        {
+            frmAmount frmAmount = new frmAmount();
+            frmAmount.ShowDialog();
+        }
+    }
+}
